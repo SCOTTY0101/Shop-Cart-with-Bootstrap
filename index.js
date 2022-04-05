c1cfd106838d93ca8a7676721870af16fda39df3
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2022-04-05 21:27:46 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+//Check local storge is browser support.
+
+function CheckBrowser() {
+    if ('localStorage' in window && window['localStorage'] !==null) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//console.log(CheckBrowser()); //comment out for testing
+
+function doShowAll() {
+    if (CheckBrowser()) {
+        let key = '';
+        let list = "<tr><th>Item</th><th>Value</th></tr>\n";
+       // let i = 0;
+
+        for (let i = 0 ; i <= localStorage.length-1; i++) {
+            key =localStorage.key(i);
+            list += "<tr><td>" + key + "</td>\n<td>"
+             + localStorage.getItem(key) + "</td></tr>\n";
+        }
+
+        if ( list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        } document.getElementById('list').innerHTML = list;
+
+    } else {
+        alert('Cannot save shopping list to your browser. Not supported by HTML5');
+    }
+};
+
+function SaveItem() {
+    let name = document.forms.ShoppingList.name.value;
+    let data = document.forms.ShoppingList.data.value;
+    localStorage.setItem(name, data);
+    doShowAll();
+};
+
+function ModifyItem() {
+    let name1 = document.forms.ShoppingList.name.value;
+    let data1 = document.forms.ShoppingList.data.value;
+
+    if (localStorage.getItem(name1) !=null) {
+            localStorage.setItem(name1, data1);
+            document.forms.ShoppingList.data.value = localStorage.getItem(name1);
+    }
+    doShowAll();
+};
+
+function RemoveItem() {
+    let name = document.forms.ShoppingList.name.vale;
+    document.forms.ShoppingList.data.value = localStorage.removeItem(name);
+    doShowAll();
+};
+
+function ClearAll() { 
+    localStorage.clear();
+    doShowAll();  
+};
